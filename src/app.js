1be26f26abd68553f00a1c5589abceadae328227
Casto Gil <casto782@gmail.com>
2023-07-23 19:43:31 -0300

    proyecto final

--- src/app.js
+++ src/app.js
@@ -87,7 +87,7 @@ app.use((req, res, next) => {
   if (req.url === "/") {
     return res.redirect("/api/products");
   }
-  next();
+  next()
 });
 app.use("/api", mainRouter); // Manejador de rutas
 app.use("/", viewsRouter); // Ruta websocket
